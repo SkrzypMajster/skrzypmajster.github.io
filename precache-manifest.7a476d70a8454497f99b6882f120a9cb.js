e587a873ceccd151a9834384b81654a9a08ccd7b
SkrzypMajster <skrzypmajster@gmail.com>
2019-10-02 17:16:53 +0200

    Deploy commit 8dd37f5b5e4e7c2c9f790dd68fb55d7a3e928171

---
+++ precache-manifest.7a476d70a8454497f99b6882f120a9cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9460f3e787bae5b235dac4c63c028c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a2178d0cb54183e390",
+    "url": "/static/css/main.11fb746a.chunk.css"
+  },
+  {
+    "revision": "6c89021d40c54e0f7021",
+    "url": "/static/js/2.c26b40cb.chunk.js"
+  },
+  {
+    "revision": "08a2178d0cb54183e390",
+    "url": "/static/js/main.45e488d9.chunk.js"
+  },
+  {
+    "revision": "3ebe1bb7f99b5242980f",
+    "url": "/static/js/runtime-main.376997bd.js"
+  },
+  {
+    "revision": "4735fb72003b9f03ecb12951a56f09e8",
+    "url": "/static/media/comming-soon-background.4735fb72.jpg"
+  }
+]);
