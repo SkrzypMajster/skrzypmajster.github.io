bcbefe6e3ca4040107eec142a802b8c440b4ce87
SkrzypMajster <skrzypmajster@gmail.com>
2019-10-06 20:40:22 +0200

    Create AboutComponent with styles

--- src/components/about/about.component.tsx
+++ src/components/about/about.component.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './about.component.scss';
 
-interface IProps { }
+interface IProps {
+  paragraphs: string[],
+  sentence: string,
+  imageSrc: string
+}
 
 export const AboutComponent: React.SFC<IProps> = (props: IProps) => {
+  const {
+    paragraphs,
+    sentence,
+    imageSrc
+  } = props;
+
+  const textParagraphs: JSX.Element[] = renderParagraphs(paragraphs);
+
   return (
-    <div>
-      About component works.
-    </div>
+    <section className="about" id="about">
+      <h3 className="about__header">About me</h3>
+      <p className="about__sentence">{sentence}</p>
+      <div className="about__content">
+        <img className="about__image" src={imageSrc} alt="Author's image"></img>
+        <article className="about__text">
+          {textParagraphs}
+        </article>
+      </div>
+    </section>
   );
+}
+
+function renderParagraphs(paragraphs: string[]): JSX.Element[] {
+  return paragraphs.map((paragraph: string) => {
+    return <p>{paragraph}</p>
+  });
 }
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -40,13 +40,24 @@ export const HomePage: React.SFC<IProps> = (props: IProps) => {
 
   const sliderText = 'Dawid "SkrzypMajster" Skrzypczyk';
 
+  const userSentence = 'I love programming'
+  const userImageUrl = 'https://avatars3.githubusercontent.com/u/22637715?s=280';
+  const aboutTextParagraphs = [
+    'Welcome to my website. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  ];
+
   return (
     <div className="home">
       <NavbarComponent
         links={navbarLinks}/>
       <SliderComponent
         text={sliderText} />
-      <AboutComponent />
+      <AboutComponent
+        sentence={userSentence}
+        imageSrc={userImageUrl}
+        paragraphs={aboutTextParagraphs} />
       <SectionHeaderComponent />
       <ResumeComponent />
       <SectionHeaderComponent />
