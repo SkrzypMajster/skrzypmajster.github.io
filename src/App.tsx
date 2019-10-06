3f9862b63fe17f142a403b9261f53fa0f3831f0b
SkrzypMajster <skrzypmajster@gmail.com>
2019-10-06 16:56:03 +0200

    Set Home Page as application default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import './App.scss';
-import { CommingSoonPage } from './pages';
+import { HomePage } from './pages';
 
 const App: React.FC = () => {
-  const applicationName: string = 'skrzypmajster.github.io';
-  const applicationAuthorLink: string = 'https://skrzypmajster.github.io';
-  const applicationAuthorName: string = 'Dawid \'SkrzypMajster\' Skrzypczyk';
-  const messageHeader: string = 'Comming Soon';
-  const messageText: string = '... maybe in some day';
-
   return (
     <div className="App">
-      <CommingSoonPage
-        appName={applicationName}
-        authorLink={applicationAuthorLink}
-        authorName={applicationAuthorName}
-        messageHeader={messageHeader}
-        messageText={messageText} />
+      <HomePage />
     </div>
   );
 }
